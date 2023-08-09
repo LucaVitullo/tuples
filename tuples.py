291ae9b746270f5814d2a13be6c0ead2f7b42b72
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2023-08-09 14:27:16 +0300

    airport tuples + for loops

--- tuples.py
+++ tuples.py
@@ -2,3 +2,14 @@ contacts =[('Jason','555-0123'), ('carl', '555-0987')]
 
 for (name, phone) in contacts :
     print("{}'s phone number is {}.".format(name,phone))
+
+
+    airports = [("'O'HARE international Airport", "ORD"),
+                 ("Los Angeles International Airport", "LAX"),
+                   ("Dallas/Fort Worth International Airport", "DFW"),
+                   ("Denver International Airport", "DEN")
+                ]
+    
+for (airport, airport_code) in airports:
+
+    print("The code for {} is {} ." .format(airport,airport_code))
